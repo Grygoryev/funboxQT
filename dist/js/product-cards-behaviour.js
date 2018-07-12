60cfcdf52a5a4001085c271211c4480ac066f147
Nickel 13 <nikesushko@gmail.com>
2018-07-12 19:11:27 +0300

    editing js behaviour for cards

--- dist/js/product-cards-behaviour.js
+++ dist/js/product-cards-behaviour.js
@@ -28,30 +28,8 @@ $(document).ready(function () {
       cardSelected = ('product-card_selected'),
       cardSelectedHover = 'product-card_selected-hover';
 
-  //------- counter for cards -------//
-  function makeCounter() {
-    var currentCount = 0;
-
-    return {
-      next : function () {
-        return currentCount++;
-      },
-      reset : function () {
-        currentCount = 0;
-      },
-      setValue : function (value) {
-        currentCount = value;
-      },
-      current : function () {
-        return currentCount;
-      }
-    };
-  }
-
-  var counterClick = makeCounter(); //
-  var counterMouseLeave =  makeCounter(); //this will count mouseLeave events and is necessary for postponing hover+select effect
-
-  //---------------------------------//
+  var cardClicked = false;
+  var hoverPostPone = false;
 
   card.hover(function () {
     $(this).toggleClass(cardHover);
@@ -60,37 +38,24 @@ $(document).ready(function () {
   card.click(function () {
 
     $(this).toggleClass(cardSelected);
-    counterClick.setValue(2); // when card is selected for the first time, counterClick accept value multiple to 2
+    cardClicked = true;
 
-
-    console.log("mouseLeave: " +  counterMouseLeave.current() + "; selectClick: " +  counterClick.current());
-
-    //next click on card will nullify counterClick and counterMouseLeave values by reset() method
-    // That is the way to make cards behaviour(on select and select+hover) independent from each other
-    if (counterClick.current() % 2 === 0 ) {
+    if (cardClicked) {
       $(this).removeClass(cardSelectedHover);
-      counterMouseLeave.reset();
-      counterClick.reset();
+      cardClicked = false;
+      hoverPostPone = false;
     }
-
 
     $(this).mouseenter(function () {
 
-      //for the first time, when card is selected+hover, there is a checking
-      //That's way becoming possible to delay hover effect on 2nd time
-      //Further, after mouseleave event, counterMouseLeave gets value more than 0
-      //and select+hover begins to work
-      if (counterMouseLeave.current() !== 0 && $(this).hasClass(cardSelected)) {
+      if (hoverPostPone && $(this).hasClass(cardSelected)) {
         $(this).addClass(cardSelectedHover);
-        $(this).removeClass(cardHover);
       }
     });
 
     $(this).mouseleave(function () {
       $(this).removeClass(cardSelectedHover);
-      $(this).removeClass(cardHover);
-      counterMouseLeave.next();
-      console.log("mouseLeave: " +  counterMouseLeave.current());
+      hoverPostPone = true;
     });
 
   });
--- src/js/product-cards-behaviour.js
+++ src/js/product-cards-behaviour.js
@@ -28,30 +28,8 @@ $(document).ready(function () {
       cardSelected = ('product-card_selected'),
       cardSelectedHover = 'product-card_selected-hover';
 
-  //------- counter for cards -------//
-  function makeCounter() {
-    var currentCount = 0;
-
-    return {
-      next : function () {
-        return currentCount++;
-      },
-      reset : function () {
-        currentCount = 0;
-      },
-      setValue : function (value) {
-        currentCount = value;
-      },
-      current : function () {
-        return currentCount;
-      }
-    };
-  }
-
-  var counterClick = makeCounter(); //
-  var counterMouseLeave =  makeCounter(); //this will count mouseLeave events and is necessary for postponing hover+select effect
-
-  //---------------------------------//
+  var clickPermission = false;
+  var hoverPermission = false;
 
   card.hover(function () {
     $(this).toggleClass(cardHover);
@@ -60,17 +38,14 @@ $(document).ready(function () {
   card.click(function () {
 
     $(this).toggleClass(cardSelected);
-    counterClick.setValue(2); // when card is selected for the first time, counterClick accept value multiple to 2
-
-
-    console.log("mouseLeave: " +  counterMouseLeave.current() + "; selectClick: " +  counterClick.current());
+    clickPermission = true;
 
     //next click on card will nullify counterClick and counterMouseLeave values by reset() method
     // That is the way to make cards behaviour(on select and select+hover) independent from each other
-    if (counterClick.current() % 2 === 0 ) {
+    if (clickPermission) {
       $(this).removeClass(cardSelectedHover);
-      counterMouseLeave.reset();
-      counterClick.reset();
+      clickPermission = false;
+      hoverPermission = false;
     }
 
 
@@ -80,17 +55,14 @@ $(document).ready(function () {
       //That's way becoming possible to delay hover effect on 2nd time
       //Further, after mouseleave event, counterMouseLeave gets value more than 0
       //and select+hover begins to work
-      if (counterMouseLeave.current() !== 0 && $(this).hasClass(cardSelected)) {
+      if (hoverPermission && $(this).hasClass(cardSelected)) {
         $(this).addClass(cardSelectedHover);
-        $(this).removeClass(cardHover);
       }
     });
 
     $(this).mouseleave(function () {
       $(this).removeClass(cardSelectedHover);
-      $(this).removeClass(cardHover);
-      counterMouseLeave.next();
-      console.log("mouseLeave: " +  counterMouseLeave.current());
+      hoverPermission = true;
     });
 
   });
